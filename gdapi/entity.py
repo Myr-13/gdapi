78b5956a343e2d77e32e1909f10ef7f1e284e973
lso44 <85067522+Myr-13@users.noreply.github.com>
2023-07-13 00:06:57 +0500

    Triggers base

--- gdapi/entity.py
+++ gdapi/entity.py
@@ -1,13 +1,38 @@
 from .utils import b64_decrypt, b64_encrypt
 
+# Every entity
 data_key_id = 1  # Number
 data_key_x = 2  # Number
 data_key_y = 3  # Number
+data_key_groups = 57  # See docs.md (HSV Packing)
+
+# Normal tile
 data_key_color_channel = 21  # Number
-data_key_text = 31  # b64 encoded text
 data_key_use_hsv = 41  # Number (1 or 0)
 data_key_hsv = 43  # See docs.md (Groups Packing)
-data_key_groups = 57  # See docs.md (HSV Packing)
+
+# Triggers
+data_key_trigger_time = 10  # Number
+data_key_trigger_group = 51  # Number
+data_key_trigger_spawn_t = 62  # Number (1 or 0)
+data_key_trigger_spawn_mt = 87  # Number (1 or 0)
+
+# Color trigger
+data_key_ct_r = 7  # Number
+data_key_ct_g = 8  # Number
+data_key_ct_b = 9  # Number
+data_key_ct_color_channel = 23  # Number
+
+# Move trigger
+data_key_mt_x = 28  # Number
+data_key_mt_y = 29  # Number
+data_key_mt_easing_time = 85  # Float
+
+# Spawn trigger
+data_key_st_delay = 63  # Float
+
+# Text object
+data_key_text = 31  # b64 encoded text
 
 
 class Entity:
@@ -18,6 +43,7 @@ class Entity:
 
 		# Attributes
 		self.text = None
+		self.color_channel = 0
 
 	def __str__(self):
 		return f"Entity <{self.id} {self.x} {self.y}>"
@@ -46,6 +72,8 @@ class Entity:
 				self.y = value
 			elif attr == data_key_text:
 				self.text = b64_decrypt(value.encode())
+			elif attr == data_key_color_channel:
+				self.color_channel = value
 
 	def save(self):
 		data = b""
@@ -60,6 +88,8 @@ class Entity:
 				text_data = text_data.encode()
 
 			data += f",{data_key_text},{b64_encrypt(text_data).decode()}".encode()
+		if self.color_channel:
+			data += f",{data_key_color_channel},{self.color_channel}".encode()
 
 		data += b";"
 
